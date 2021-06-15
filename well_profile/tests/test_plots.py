81e5e41d9de807cb6babb58a20938e012fc723a2
jcamiloangarita <camiloang94@gmail.com>
2021-06-15 14:14:12 +0200

    Update tests

--- well_profile/tests/test_plots.py
+++ well_profile/tests/test_plots.py
@@ -2,11 +2,12 @@ from unittest import TestCase
 from well_profile import load
 
 
-class TestStyle(TestCase):
+class TestPlots(TestCase):
 
     def test_dark_mode(self):
         well = load(r'https://github.com/pro-well-plan/well_profile/raw/master/well_profile/tests/trajectory1.xlsx')
-        well.plot(style={'darkMode': True})
+        fig = well.plot(style={'darkMode': True})
+        self.assertEqual(fig.layout.template.layout.mapbox.style, 'dark')
 
     def test_color(self):
         well = load(r'https://github.com/pro-well-plan/well_profile/raw/master/well_profile/tests/trajectory1.xlsx')
@@ -15,6 +16,11 @@ class TestStyle(TestCase):
     def test_size(self):
         well = load(r'https://github.com/pro-well-plan/well_profile/raw/master/well_profile/tests/trajectory1.xlsx')
         well.plot(style={'size': 5})
+
+    def test_top_view(self):
+        well = load(r'https://github.com/pro-well-plan/well_profile/raw/master/well_profile/tests/trajectory1.xlsx')
+        fig = well.plot(plot_type='top', style={'darkMode': True})
+        self.assertEqual(fig.layout.template.layout.mapbox.style, 'dark')
 
 
 def run_assertions(self, well, mdt):
